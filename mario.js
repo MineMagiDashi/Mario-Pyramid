1919405dcdae859816622928478ed0faf51588f5
Mine <90278699+MineMagiDashi@users.noreply.github.com>
2022-01-27 19:35:28 +0000

    Initial commit

---
+++ mario.js
@@ -0,0 +1,32 @@
+let userinputHeight = prompt("Pick a height for the console pyramid: ");
+printPyramid(userinputHeight);
+// TODO #2
+// Take in user input for the height
+
+
+/*
+ * printPyramid
+ *
+ * Prints to the console a pyramid of '#' characters of the specified height
+ * For example, if height is 5, the console will look like this:
+ *          ##
+ *         ###
+ *        ####
+ *       #####
+ *      ######
+ */
+function printPyramid(height) {
+  for (let i = 0; i < height; i++)
+  {
+    let lineOfHashes = "";
+    for (let o = 0; o < height-i; o++)
+    {
+      lineOfHashes = lineOfHashes + " ";
+    }
+    for (let o = 0; o < i+1; o++)
+    {
+      lineOfHashes = lineOfHashes + "#";
+    }
+    console.log(lineOfHashes);
+  }
+}
