71787fef46127fe8241852fa9f28af58b6ef44d6
Mine <90278699+MineMagiDashi@users.noreply.github.com>
2022-02-13 22:43:07 +0000

    added HTML input thing (Mario 3)

--- mario.js
+++ mario.js
@@ -1,5 +1,5 @@
-let userinputHeight = prompt("Pick a height for the console pyramid: ");
-printPyramid(userinputHeight);
+//let userinputHeight = prompt("Pick a height for the console pyramid: ");
+//printPyramid(userinputHeight);
 
 
 
@@ -12,7 +12,6 @@ function printPyramid(height) {
     for (let o = 0; o < i + 1; o++) {
       rowStr = rowStr + "#";
     }
-    console.log(rowStr);
     const para = document.createElement("p");
     const node = document.createTextNode(rowStr);
     para.appendChild(node);
@@ -21,6 +20,14 @@ function printPyramid(height) {
   }
 }
 
-// TODO 1
-  // create an element whose inner text is rowStr,
-  // and insert it as a child of the container <div id="pyramid">
+
+function determineHeightAndThenDrawPyramid(){
+  let inputVal = document.getElementById("height").value;
+  printPyramid(inputVal);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+  console.log("Button clicked.");
+  determineHeightAndThenDrawPyramid();
+});
